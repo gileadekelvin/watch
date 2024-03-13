8749dbc9400d077f8de0b000bc23989429955be6
Gileade Kelvin <gileadekelvin@gmail.com>
2024-03-13 09:26:06 -0300

    feat: migrate from planetscale(mysql) to turso(sqlite)

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,12 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
 import { env } from "~/env.mjs";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
+const libsql = createClient({
+  url: `${env.TURSO_DATABASE_URL}`,
+  authToken: `${env.TURSO_AUTH_TOKEN}`,
+});
+
+const adapter = new PrismaLibSQL(libsql);
+
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
